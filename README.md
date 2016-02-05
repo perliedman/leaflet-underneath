fc7ef83ab7beba466fea73e9c67ab1d2e5ac30f7
perliedman <per.liedman@dotnetmentor.se>
2016-02-05 16:31:01 +0100

    First mostly working version!

---
+++ README.md
@@ -0,0 +1,11 @@
+# Leaflet Underneath
+
+Find features or points of interest that lie underneath your mouse pointer (or some other location).
+
+With a normal tile layer, the user can't interact to find out more about a location, since it is a static image. With this plugin, you can find out what features are underneath the current mouse position, for example when the user clicks the map.
+
+This is done using [Mapbox Vector Tiles](https://www.mapbox.com/developers/vector-tiles/), that are queried for features in a way that is both fast and reasonably bandwidth efficient.
+
+[Check out the demo](http://www.liedman.net/leaflet-underneath/)
+
+(Note: while this is mostly working, it's currently work-in-progress: feedback and comments welcome, try it out, but use it for serious stuff on your own risk.)
--- src/index.js
+++ src/index.js
@@ -1,45 +1,86 @@
 var Protobuf = require('pbf'),
     VectorTile = require('vector-tile').VectorTile,
     L = require('leaflet'),
-    corslite = require('corslite');
+    corslite = require('corslite'),
+    rbush = require('rbush');
 
 module.exports = L.TileLayer.Pois = L.TileLayer.extend({
     options: {
-        layers: ['poi_label'],
-        maxScalerank: 2
+        layers: [],
+        defaultTolerance: 100,
+        isDuplicate: function(f, context) {
+            var osmId = f.properties.osm_id,
+                name = f.properties.name,
+                isDupe = context[osmId] || context[name];
+
+            if (!isDupe) {
+                context[osmId] = true;
+                context[name] = true;
+            }
+
+            return isDupe;
+        }
     },
 
     initialize: function(tileUrl, options) {
         L.TileLayer.prototype.initialize.call(this, tileUrl, options);
-        this._featureLayer = L.geoJson(null, {
-            onEachFeature: function(f, l) {
-                l.bindPopup('<h3>' + f.properties.name + '</h3>');
+        this._bush = rbush(this.options.rbushMaxEntries);
+    },
+
+    query: function(latLng, tolerance) {
+        if (!this._map) return;
+
+        tolerance = tolerance || this.options.defaultTolerance;
+
+        var p = this._map.latLngToLayerPoint(latLng),
+            sqDistToP = function(s) {
+                var dx = s[0] - p.x,
+                    dy = s[1] - p.y;
+                return dx * dx + dy * dy;
+            },
+            halfT = tolerance / 2,
+            search = this._bush.search([p.x - halfT, p.y - halfT, p.x + halfT, p.y + halfT]),
+            results = [],
+            context = {},
+            isDuplicate = this.options.isDuplicate,
+            i,
+            f;
+
+        search.sort(function(a, b) { return sqDistToP(a) - sqDistToP(b); });
+
+        for (i = 0; i < search.length; i++) {
+            f = search[i][4];
+            if (!isDuplicate(f, context)) {
+                results.push(f);
             }
-        });
-    },
+        }
 
-    onAdd: function(map) {
-        L.TileLayer.prototype.onAdd.call(this, map);
-        this._featureLayer.addTo(map);
-    },
-
-    onRemove: function(map) {
-        L.TileLayer.prototype.onRemove.call(this, map);
-        map.removeLayer(this._featureLayer);
+        return results;
     },
 
     _addTile: function(tilePoint) {
-        var tile = { datum: null, processed: false };
-        this._tiles[tilePoint.x + ':' + tilePoint.y] = tile;
-        this._loadTile(tile, tilePoint);
+        var key = tilePoint.x + ':' + tilePoint.y,
+            tile = { datum: null, processed: false };
+
+        if (!this._tiles[key]) {
+            this._tiles[key] = tile;
+            this._loadTile(tile, tilePoint);
+        }
     },
 
     _loadTile: function(tile, tilePoint) {
+        var url,
+            request;
+
         this._adjustTilePoint(tilePoint);
-        var request = corslite(this.getTileUrl(tilePoint), L.bind(function(err, data) {
+        url = this.getTileUrl(tilePoint);
+        request = corslite(url, L.bind(function(err, data) {
             if (err) {
-                // TODO: error handling
-                return console.log(err);
+                this.fire('tileerror', {
+                    tilePoint: tilePoint,
+                    url: url,
+                    error: err
+                });
             }
 
             this._tileLoaded(tile, tilePoint, new Uint8Array(data.response));
@@ -49,30 +90,47 @@ module.exports = L.TileLayer.Pois = L.TileLayer.extend({
 
     _reset: function() {
         L.TileLayer.prototype._reset.call(this);
-        this._featureLayer.clearLayers();
+        this._bush.clear();
+        this.fire('featurescleared');
     },
 
     _tileLoaded: function(tile, tilePoint, data) {
         var x = tilePoint.x,
             y = tilePoint.y,
             z = tilePoint.z,
-            maxScalerank = this.options.maxScalerank,
+            vectorTile = new VectorTile(new Protobuf(data)),
+            filter = this.options.filter,
             i,
             j,
             layerName,
             layer,
-            f;
+            f,
+            p,
+            geojson;
 
-        tile.datum = new VectorTile(new Protobuf(data));
+        tile.processed = true;
 
         for (i = 0; i < this.options.layers.length; i++) {
             layerName = this.options.layers[i];
-            layer = tile.datum.layers[layerName];
+            layer = vectorTile.layers[layerName];
             if (layer) {
                 for (j = 0; j < layer.length; j++) {
                     f = layer.feature(j);
-                    if (f.properties['scalerank'] <= maxScalerank) {
-                        this._featureLayer.addData(f.toGeoJSON(x, y, z));
+                    if (!filter || filter(f)) {
+                        geojson = f.toGeoJSON(x, y, z);
+                        if (geojson.geometry.type !== 'Point') {
+                            this.fire('featureerror', {
+                                tilepoint: tilePoint,
+                                error: 'Feature does not have a point geometry',
+                                feature: f
+                            });
+                            continue;
+                        }
+                        p = this._map.latLngToLayerPoint([geojson.geometry.coordinates[1], geojson.geometry.coordinates[0]]);
+                        this._bush.insert([p.x, p.y, p.x, p.y, geojson]);
+                        this.fire('featureadded', {
+                            feature: geojson
+                        });
                     }
                 }
             }
